ba0f2852b356bb0168b40e5683913a83b1358ea3
bobbyhadz <opmftw@gmail.com>
2024-04-05 17:27:13 +0300

    minor changes

---
+++ index.js
@@ -0,0 +1,27 @@
+// 👇️ Using ES6 modules import/export syntax
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'bobbyhadz.com',
+    message: 'Example message: bobbyhadz.com',
+    salary: 500,
+  });
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
